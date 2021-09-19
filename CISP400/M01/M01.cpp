566304cb48533324fbfa952228b91b08f9b311f0
Jerry-Shu <54721945+Jerry-Shu@users.noreply.github.com>
2021-09-18 21:06:09 -0700

    Create M01.cpp

---
+++ CISP400/M01/M01.cpp
@@ -0,0 +1,31 @@
+#include "../std_lib_facilities.h"
+#include "M01.h"
+
+using namespace std;
+double multiplication (double operands1, double operands2)
+{
+
+    return (operands1 * operands2);
+
+
+}
+double division (double operands1, double operands2)
+{
+
+
+    return (operands1 / operands2);
+
+}
+double subtraction (double operands1, double operands2)
+{
+
+    return (operands1 - operands2);
+
+
+}
+double addition (double operands1, double operands2)
+{
+
+    return (operands1 + operands2);
+
+}
