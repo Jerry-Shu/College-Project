3155651d7df7d2891b699a57300d74b81b66f8c9
Jerry-Shu <54721945+Jerry-Shu@users.noreply.github.com>
2021-09-18 21:06:26 -0700

    Create M01.h

---
+++ CISP400/M01/M01.h
@@ -0,0 +1,4 @@
+double multiplication (double operands1, double operands2);
+double division (double operands1, double operands2);
+double subtraction (double operands1, double operands2);
+double addition (double operands1, double operands2);
