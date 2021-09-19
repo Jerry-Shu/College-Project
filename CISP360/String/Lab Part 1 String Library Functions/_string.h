88c90a5264cbac3bd6784451126886c64a27632a
Jerry-Shu <54721945+Jerry-Shu@users.noreply.github.com>
2021-09-18 19:14:05 -0700

    Create _string.h

---
+++ CISP360/String/Lab Part 1 String Library Functions/_string.h
@@ -0,0 +1,15 @@
+#ifndef _STRING_H_INCLUDED
+#define _STRING_H_INCLUDED
+
+#define  strlen _strlen
+#define  strcmp _strcmp
+#define  strcpy _strcpy
+#define  strncpy _strncpy
+#define  strcat _strcat
+#define  strncat _strncat
+#define  strchr _strchr
+#define  strrchr _strrchr
+#define  strstr _strstr
+#define  isalnum _isalnum
+
+#endif // _STRING_H_INCLUDED
