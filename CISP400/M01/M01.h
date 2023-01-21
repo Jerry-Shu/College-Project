3f9bfd6cd06e5ed722a77aa362b1efe07e125c40
Jerry-Shu <54721945+Jerry-Shu@users.noreply.github.com>
2023-01-20 19:51:47 -0800

    Delete CISP400/M01 directory

--- CISP400/M01/M01.h
+++
@@ -1,4 +0,0 @@
-double multiplication (double operands1, double operands2);
-double division (double operands1, double operands2);
-double subtraction (double operands1, double operands2);
-double addition (double operands1, double operands2);
